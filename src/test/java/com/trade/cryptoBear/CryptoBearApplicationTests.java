cd152a13e85affd30b8af42f8ad445408a7f4287
tin_hong_wen <e03256506@u.nus.edu>
2025-04-17 22:55:50 +0800

    first commit

---
+++ src/test/java/com/trade/cryptoBear/CryptoBearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.cryptoBear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
